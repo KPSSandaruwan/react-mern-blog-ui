881e18fcc23e4810cf51953565300d7b7df2e8b7
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2021-10-31 21:49:15 +0530

    Add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//Pages
+import { About } from './components/About';
+import { ArticlesList } from './components/ArticlesList';
+import { Article } from './components/Article';
+import { Home } from './components/Home';
+
 
 function App() {
   return (
-    <div className="container bg-blue-500">
-      App Component
+    <div className="max-w-screen mx-auto">
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/article-list" component={ArticlesList} />
+        <Route exact path="/article" component={Article} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="mt-40 mb-20 pl-48 pr-48">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-10 text-gray-900">
+        Hello, Welcome to my Blog!
+      </h1>
+      <p className="mx-auto leading-relax text-base mb-4 text-gray-800 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est beatae
+        quo harum pariatur iste, vel, sunt porro, doloribus aperiam consequuntur
+        laboriosam asperiores? Eum autem rerum vel aliquid ratione nobis.
+      </p>
+
+      <p className="mx-auto leading-relax text-base mb-4 text-gray-800 text-justify">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur ipsam,
+        recusandae dolorem, quam ea molestias iure impedit omnis voluptatum
+        vitae quidem consequatur? Aperiam ex eum veniam deleniti quibusdam
+        molestias ea harum, dolores asperiores excepturi necessitatibus quaerat
+        alias doloremque maiores commodi quae ipsum non eveniet vel voluptatibus
+        perferendis. Impedit, rerum est?
+      </p>
+      <p className="mx-auto leading-relax text-base mb-4 text-gray-800 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae porro
+        perspiciatis animi id quo fugit quaerat iure eius placeat iste. Dolore,
+        asperiores vero. Quia quaerat eius blanditiis! Illo, provident
+        consequuntur?
+      </p>
+    </div>
+  )
+}
